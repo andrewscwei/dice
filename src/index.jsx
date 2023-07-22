2aafc5387fffeed8a9975e3c2a4046d7cdf6a0f5
Andrew Wei <andrew@sybl.io>
2023-07-22 10:36:55 -0700

    Upgrade React

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './containers/App';
 
-const markup = () => (
-  <App/>
-);
-
-render(markup(), document.getElementById('app'));
+createRoot(document.getElementById('app')).render(<App/>)
