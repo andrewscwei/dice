b564570bb09891d136fbede728acf7b1c8e8e6dd
Andrew Wei <andrew@sybl.io>
2023-07-23 10:59:50 -0700

    Handle hover when not touch device

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import isTouchDevice from 'is-touch-device';
 
 window.__VERSION__ = $APP_CONFIG.version;
+
+if (isTouchDevice()) {
+  document.documentElement.classList.add('touch');
+  document.documentElement.classList.remove('no-touch');
+}
+else {
+  document.documentElement.classList.remove('touch');
+  document.documentElement.classList.add('no-touch');
+}
 
 createRoot(document.getElementById('app')).render(<App/>);
